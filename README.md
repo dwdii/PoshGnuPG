d9342833a21fd536b3e8fd5501d38591e502c9f0
dwdii <daniel@dittenhafer.net>
2018-11-09 21:30:56 -0500

    doc updates

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # PoshGnuPG
-Powershell wrapper for Gpg4win and GnuGP
+Powershell wrapper for [Gpg4win](https://www.gpg4win.org/) and GnuGP
+
+## Gpg4Win
+
+This module was developed and tested using Gpg4Win v3.1.4 and GnuGP v2.2.10
+
+## Usage
 
 ```
 GpgEncrypt-File -FilePath "filepath\to\file.ext" -ForUser "user@email.com"
--- test/Test-GpgEncryptFile.ps1
+++ test/Test-GpgEncryptFile.ps1
@@ -1,27 +1,9 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-
 Import-Module ($scriptPath  + "\..\src\PoshGnuPG.ps1")
 
-if (1 -eq 0)
-{
-    $args = ( "-o", "C:\Users\Dan\OneDrive\Backups\gpg4win\2018-KeyPair-hotmail-MSI.p12",
-              "--export", "dan_dittenhafer@hotmail.com", "--export-format", "pkcs12", "--cert")
-    $args2= ( "-o", "C:\Users\Dan\OneDrive\Backups\gpg4win\2018-KeyPair-hotmail-MSI.p12",
-              "--export", "dan_dittenhafer@hotmail.com", "--export-format", "pkcs12", "--cert")
-
-    $listArgs = @("--list-secret=keys", "--keyid-format", "short")
-    $helpArgs = ("-h")
-
-    $gpgLog = "~/gpg.log"
-    $gpgErr = "~/gpgErr.log"
-
-    Start-Process -FilePath  $GpgPath -ArgumentList $helpArgs  -Wait -NoNewWindow -RedirectStandardOutput $gpgLog -RedirectStandardError $gpgErr
-
-    Get-Content $gpgLog 
-}
-
+# Get and Show Gpg.exe help
 Invoke-GpgExe -ArgumentList ("-h") -Verbose
 
-#
+# Test the encryption function
 GpgEncrypt-File -FilePath "D:\Code\PowerShell\PoshGnuPG\test-data\monalisa-test.jpg" -ForUser "daniel@dittenhafer.net" -Verbose
                 
